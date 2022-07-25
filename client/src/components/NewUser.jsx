0267f1b491fb195deacd65d406a734347a46d672
Shivam Wadhawan <shivamwadhawan2000@gmail.com>
2022-07-25 20:41:31 +0530

    youtube

--- client/src/components/NewUser.jsx
+++ client/src/components/NewUser.jsx
@@ -1,25 +1,79 @@
 import { TextField, Button } from '@mui/material'
+import axios from 'axios'
 import React from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react'
 
-const NewUser = () => {
-  const [userInfo, setUserInfo] = useState({ userName: '', emailId: '', password: '' })
-  const handleSubmit = () => {
+const createUserUrl = "http://localhost:8080/api/user/createUser"
 
+const NewUser = () => {
+  const [userInfo, setUserInfo] = useState({
+    userName: '',
+    emailId: '',
+    userPassword: ''
+  });
+
+  const errorObj = {
+    userNameErr: false,
+    emailIdErr: false,
+    userPasswordErr: false
+  };
+
+
+
+  console.log([userInfo.userPassword.length,
+  userInfo.userName.length,
+  userInfo.emailId.length])
+
+
+  const handleSubmit = async () => {
+    if (userInfo.userPassword.length === 0)
+      errorObj.userPasswordErr = true
+    else
+      errorObj.userPasswordErr = false
+    if (userInfo.userName.length === 0)
+      errorObj.userNameErr = true
+    else
+      errorObj.userNameErr = false
+    if (userInfo.emailId.length === 0)
+      errorObj.emailIdErr = true
+    else
+      errorObj.emailIdErr = false
+
+    //     else {
+    //   let config = {
+    //     url: createUserUrl,
+    //     method: 'POST',
+    //     data: {
+    //       userName: userInfo.userName,
+    //       userEmail: userInfo.emailId,
+    //       userPassword: userInfo.userPassword
+    //     }
+    //   }
+
+    //   let response = await axios(config).then((response) => {
+    //     if (response.status === 200)
+    //       return response
+    //   }).catch((err) => {
+    //     console.log(err)
+    //   })
+
+    //   localStorage.setItem('userId', response.data.id)
+    //   console.log(response.data)
+    // }
   }
+
+
   return (
     <div className="signup-dialog">
-      <TextField className='text-field-custom' label='User Name' onChange={(e) => {
+      <TextField className='text-field-custom' label='User Name' error={errorObj.userNameErr} onChange={(e) => {
         setUserInfo(prevState => ({ ...userInfo, userName: e.target.value }))
-        console.log(userInfo)
       }} />
-      <TextField className='text-field-custom' label='Email Id' onChange={(e) => {
+      <TextField className='text-field-custom' label='Email Id' error={errorObj.emailIdErr} onChange={(e) => {
         setUserInfo(prevState => ({ ...userInfo, emailId: e.target.value }))
-        console.log(userInfo)
       }} />
-      <TextField className='text-field-custom' type='password' label='Password' onChange={(e) => {
-        setUserInfo(prevState => ({ ...userInfo, password: e.target.value }))
-        console.log(userInfo)
+      <TextField className='text-field-custom' type='password' label='Password' error={errorObj.userPasswordErr} onChange={(e) => {
+        setUserInfo(prevState => ({ ...userInfo, userPassword: e.target.value }))
       }} />
 
       <Button variant='contained' color='primary' onClick={handleSubmit}>
